af0339114e39ce71b11d0dd50f031adc6d31e8f5
Benjamin Holland <ben.holland.core@gmail.com>
2018-05-01 22:15:10 -0700

    changed the chat serve to use an environment variable

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import * as io from 'socket.io-client';
 import {Observable} from 'rxjs/Observable';
 import {UserService} from '../user-service/user.service';
+import {environment} from "../../../environments/environment";
 
 export interface Message {
   message: string;
@@ -16,14 +17,12 @@ export class ChatService {
 
   constructor(private userService: UserService) {
     console.log('in constructor');
-    this.socket = io.connect('https://cst438-group-jabb.herokuapp.com:3000');
-
-
+    this.socket = io.connect(`https://${environment.domain}:3000`);
   }
 
 
   sendMessage(message: string) {
-    var msg = {
+    const msg = {
       message: message,
       username: this.userService.getUser().displayName
     };
