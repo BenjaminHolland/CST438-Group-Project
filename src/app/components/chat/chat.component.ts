64bd739a5e43533b73b959c6c61d2494fd0d85db
Benjamin Holland <ben.holland.core@gmail.com>
2018-05-02 02:22:43 -0700

    removed useless scroll sub (for now)

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,83 +1,55 @@
 import {AfterViewChecked, ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
 import {ChatService} from '../../services/chat/chat.service';
-import {FormControl} from "@angular/forms";
-import {environment} from "../../../environments/environment";
-import * as moment from 'moment'
-import {Message} from "../../models/Message";
-import {CdkScrollable, ScrollDispatcher} from "@angular/cdk/scrolling";
+import {FormControl} from '@angular/forms';
+import {environment} from '../../../environments/environment';
+import * as moment from 'moment';
+import {Message} from '../../models/Message';
+import {CdkScrollable, ScrollDispatcher} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, AfterViewChecked {
+/*
+Chat Component
+Encapsulates the chat box. Provides auto-scrolling capabilities and chat history.
+ */
+export class ChatComponent implements OnInit {
 
-  public messages: Message[];
-  newMessage: FormControl;
-
-  constructor(private chatService: ChatService, private scrollDispatch: ScrollDispatcher, private zone: NgZone, private changeDetector: ChangeDetectorRef) {
-    this.messages = [];
-    this.newMessage = new FormControl();
-
-
-  }
+  public messages: Message[] = [];
+  public newMessage: FormControl = new FormControl();
 
   @ViewChild('chat', {read: ElementRef}) private chatLog: ElementRef;
-  private chatLogScrollable: CdkScrollable;
-  private shouldScroll: boolean = false;
+
+  //Added this to prepare for intercepting user-generated scroll events,
+  //with the intention of automatically controlling the auto-scroll feature.
   @ViewChild('chat', {read: CdkScrollable}) private chatScrollable: CdkScrollable;
 
+  constructor(private chatService: ChatService) {
+  }
+
   ngOnInit() {
-    this.chatScrollable.elementScrolled().subscribe(x => {
-      console.log(x);
-    });
+
     this.chatService
       .onMessage()
       .subscribe(msg => {
         this.messages.push(msg);
-        this.changeDetector.detectChanges();
-        this.shouldScroll=true;
         this.scrollToBottom();
       });
 
   }
 
-  onScrolled(event): void {
-    console.log(event);
-  }
-
-  ngAfterViewChecked() {
-    this.scrollToBottom();
-  }
-
-  //scrolls chat to bottom of div
   scrollToBottom(): void {
-    if (this.shouldScroll) {
-      this.chatLog.nativeElement.scrollTo({left: 0, top: this.chatLog.nativeElement.scrollHeight, behavior: 'smooth'});
-      this.shouldScroll = false;
-    }
+
+    //This animates the scrolling rather than simply jumping to the next position.
+    this.chatLog.nativeElement.scrollTo({left: 0, top: this.chatLog.nativeElement.scrollHeight, behavior: 'smooth'});
   }
 
-
-  //decides if enough messages are in char to need to scroll
-  chatShouldScroll(): boolean {
-    return this.chatLog.nativeElement.scrollTop + this.chatLog.nativeElement.clientHeight === this.chatLog.nativeElement.scrollHeight;
-  }
 
   submitMessage(): void {
-
-    //send message to server
     this.chatService.sendMessage(this.newMessage.value);
-
-    //scroll chat down
-
-
-    //reset message input
     this.newMessage.setValue('');
-
-
-
   }
 
 }
