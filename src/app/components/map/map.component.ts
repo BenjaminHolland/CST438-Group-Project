e2b9a5195cd7d4bb87eaa8d77de285bdf1e0daac
Benjamin Holland <ben.holland.core@gmail.com>
2018-05-01 20:48:16 -0700

    got everything building

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, Input, OnInit, ElementRef, NgZone, ViewChild } from '@angular/core';
+import {Component, EventEmitter, Output, Input, OnInit, ElementRef, NgZone, ViewChild} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {} from 'googlemaps';
 import {MapsAPILoader} from '@agm/core';
@@ -18,7 +18,6 @@ declare var require: any;
 })
 
 
-
 export class MapComponent implements OnInit {
 
 //emitter than sends a subscribable event when a new locatino
@@ -27,12 +26,10 @@ export class MapComponent implements OnInit {
   @Output() onLocation = new EventEmitter<any>();
 
 
-
   @Input() showLocations: boolean = true;
   @Input() showSearchMarker: boolean = false;
-  @Input() inputLat:number = null;
-  @Input() inputLng:number = null;
-
+  @Input() inputLat: number = null;
+  @Input() inputLng: number = null;
 
 
   title: string = 'My first AGM project';
@@ -69,158 +66,153 @@ export class MapComponent implements OnInit {
 
 
   constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone,
+    public router: Router,
+    private mapService: MapService,
+    private userService: UserService,
+    private teamService: TeamService
+  ) {
 
-  	private mapsAPILoader: MapsAPILoader,
-  	private ngZone: NgZone,
-  	private router: Router,
-  	private mapService: MapService,
-  	private userService: UserService,
-  	private teamService: TeamService
-  	) {
+    this.filter = 'all';
+    this.showFilter = true;
 
-  	this.filter = 'all';
-  	this.showFilter = true;
+    this.placeService = null;
 
-  	this.placeService = null;
+    this.dataLoaded = false;
 
-  	this.dataLoaded = false;
+    this.poiList = new Array();
 
-  	this.poiList = new Array();
+    this.homeCoords = {
+      lat: 36.63536611993544,
+      lng: -121.81724309921265
+    };
 
-  	this.homeCoords = {
-  		lat: 36.63536611993544,
-  		lng: -121.81724309921265
-  	};
-
-  	this.searchMarker = {
-  		lat: this.homeCoords.lat - 0.05,
-  		lng: this.homeCoords.lng + 0.04,
-  		isOpen: this.showSearchMarker,
-  		text: "Drag Me"
-  	};
-
+    this.searchMarker = {
+      lat: this.homeCoords.lat - 0.05,
+      lng: this.homeCoords.lng + 0.04,
+      isOpen: this.showSearchMarker,
+      text: "Drag Me"
+    };
 
 
+    this.currentSearchLocation = {
+      lat: 0.0,
+      lng: 0.0
+    };
 
-  	this.currentSearchLocation = {
-  		lat: 0.0,
-  		lng: 0.0
-  	};
-
-  	this.markers = new Array();
+    this.markers = new Array();
 
 
-  	this.zoom = 10;
-  	this.lat = 36.63536611993544;
-    this.lng =  -121.81724309921265;
+    this.zoom = 10;
+    this.lat = 36.63536611993544;
+    this.lng = -121.81724309921265;
 
     if (this.inputLat && this.inputLng) {
-  		this.lat = this.inputLat;
-  		this.lng = this.inputLng;
+      this.lat = this.inputLat;
+      this.lng = this.inputLng;
 
-  		this.searchMarker.lat = this.inputLat;
-  		this.searchMarker.lng = this.inputLng;
-  		this.getLocationInfo(this.inputLat, this.inputLng, (error,data) => {
-  			this.searchMarker.text = data.name;
-  		});
-  	}
-
-
-  	//building map objec to pass into map selector in html
-  	this.map = {
-  		center: {
-  			lat: 0,
-  			lng: 0
-  		},
-  		zoom: this.zoom,
-  		mapTypeId: this.mapType,
-  		styles: [
-
-			{
-				"featureType": "landscape",
-				"stylers": [{"visibility":"off"}]
-			},
-			{
-				"featureType": "poi.attraction",
-				"stylers": [{"visibility":"off"}]
-			},
-			{
-				"featureType": "poi.business",
-				"stylers": [{"visibility":"off"}]
-			},
-			{
-				"featureType": "poi.government",
-				"stylers": [{"visibility":"off"}]
-			},
-			{
-				"featureType": "poi.medical",
-				"stylers": [{"visibility":"off"}]
-			},
-			{
-				"featureType": "poi.park",
-				"stylers": [{"visibility":"on"}]
-			},
-			{
-				"featureType": "poi.place_of_worship",
-				"stylers": [{"visibility":"off"}]
-			},
-			{
-				"featureType": "poi.school",
-				"stylers": [{"visibility":"on"}]
-			},
-			{
-				"featureType": "poi.sports_complex",
-				"stylers": [{"visibility":"on"}]
-			},
-			{
-				"featureType": "transit",
-				"stylers": [{"visibility":"off"}]
-			}
-  		]
-  	};
+      this.searchMarker.lat = this.inputLat;
+      this.searchMarker.lng = this.inputLng;
+      this.getLocationInfo(this.inputLat, this.inputLng, (error, data) => {
+        this.searchMarker.text = data.name;
+      });
+    }
 
 
+    //building map objec to pass into map selector in html
+    this.map = {
+      center: {
+        lat: 0,
+        lng: 0
+      },
+      zoom: this.zoom,
+      mapTypeId: this.mapType,
+      styles: [
 
-  	this.markerSubscription = this.mapService.getSearchMarkerEvent().subscribe(res => {
+        {
+          "featureType": "landscape",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.attraction",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.business",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.government",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.medical",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.park",
+          "stylers": [{"visibility": "on"}]
+        },
+        {
+          "featureType": "poi.place_of_worship",
+          "stylers": [{"visibility": "off"}]
+        },
+        {
+          "featureType": "poi.school",
+          "stylers": [{"visibility": "on"}]
+        },
+        {
+          "featureType": "poi.sports_complex",
+          "stylers": [{"visibility": "on"}]
+        },
+        {
+          "featureType": "transit",
+          "stylers": [{"visibility": "off"}]
+        }
+      ]
+    };
 
-		this.searchMarker.isOpen = res.marker.isOpen;
 
-		if (this.showSearchMarker) {
-			this.searchMarker.isOpen = true;
-		}
+    this.markerSubscription = this.mapService.getSearchMarkerEvent().subscribe(res => {
 
-		if (res.marker.isOpen == true) {
-			this.searchMarker.lat = this.lat;
-			this.searchMarker.lng = this.lng;
-		}
-  	});
+      this.searchMarker.isOpen = res.marker.isOpen;
+
+      if (this.showSearchMarker) {
+        this.searchMarker.isOpen = true;
+      }
+
+      if (res.marker.isOpen == true) {
+        this.searchMarker.lat = this.lat;
+        this.searchMarker.lng = this.lng;
+      }
+    });
 
   }
 
   ngOnInit() {
 
 
-    this.mapService.downloadLocations( (data) => {
-    	this.markers = data;
+    this.mapService.downloadLocations((data) => {
+      this.markers = data;
 
-  		for (var i = 0; i < this.markers.length;i++) {
-  			console.log(this.markers[i]);
-  			var latStr = this.markers[i].coords.lat;
-  			var latNum = parseFloat(latStr);
-  			var lngStr = this.markers[i].coords.lng;
-  			var lngNum = parseFloat(lngStr);
-  			this.markers[i].coords = {
-  				lat: latNum,
-  				lng: lngNum
-  			};
-  		}
-  		this.dataLoaded = true;
-  	});
+      for (var i = 0; i < this.markers.length; i++) {
+        console.log(this.markers[i]);
+        var latStr = this.markers[i].coords.lat;
+        var latNum = parseFloat(latStr);
+        var lngStr = this.markers[i].coords.lng;
+        var lngNum = parseFloat(lngStr);
+        this.markers[i].coords = {
+          lat: latNum,
+          lng: lngNum
+        };
+      }
+      this.dataLoaded = true;
+    });
 
-	this.searchMarker.isOpen = this.showSearchMarker;
+    this.searchMarker.isOpen = this.showSearchMarker;
 
-  	this.searchControl = new FormControl();
-
+    this.searchControl = new FormControl();
 
 
     this.searchControl = new FormControl();
@@ -260,32 +252,32 @@ export class MapComponent implements OnInit {
             this.searchMarker.text = place.name;
 
             if (this.inputLat && this.inputLng) {
-  		this.lat = this.inputLat;
-  		this.lng = this.inputLng;
+              this.lat = this.inputLat;
+              this.lng = this.inputLng;
 
-  		this.searchMarker.lat = this.inputLat;
-  		this.searchMarker.lng = this.inputLng;
-  		this.getLocationInfo(this.inputLat, this.inputLng, (error,data) => {
+              this.searchMarker.lat = this.inputLat;
+              this.searchMarker.lng = this.inputLng;
+              this.getLocationInfo(this.inputLat, this.inputLng, (error, data) => {
 
-  		});
-  	}
+              });
+            }
           });
         });
       });
     });
 
     if (this.inputLat && this.inputLng) {
-  		this.lat = this.inputLat;
-  		this.lng = this.inputLng;
+      this.lat = this.inputLat;
+      this.lng = this.inputLng;
 
-  		this.searchMarker.lat = this.inputLat;
-  		this.searchMarker.lng = this.inputLng;
-  		this.getLocationInfo(this.inputLat, this.inputLng, (error,data) => {
-  			if (data && data.name) {
-  				this.searchMarker.text = data.name;
-  			}
-  		});
-  	}
+      this.searchMarker.lat = this.inputLat;
+      this.searchMarker.lng = this.inputLng;
+      this.getLocationInfo(this.inputLat, this.inputLng, (error, data) => {
+        if (data && data.name) {
+          this.searchMarker.text = data.name;
+        }
+      });
+    }
 
 
   }
@@ -448,10 +440,10 @@ export class MapComponent implements OnInit {
           }
 
 
-				this.onLocation.emit(data);
-  				this.mapService.emitClickEvent(data);
-  				callback(null,data);
-   		    }
+          this.onLocation.emit(data);
+          this.mapService.emitClickEvent(data);
+          callback(null, data);
+        }
 
       }
       callback('error', null);
@@ -539,12 +531,12 @@ export class MapComponent implements OnInit {
     this.router.navigate(['map/locations', id]);
   }
 
-  getTrophyIcon(){
-  	return require('../../../images/trophy.png');
+  getTrophyIcon() {
+    return require('../../../images/trophy.png');
   }
 
   challengeBtnPressed(locId, teamId) {
-  	this.router.navigate(['/teams/match', {locId: locId, teamId:teamId}]);
+    this.router.navigate(['/teams/match', {locId: locId, teamId: teamId}]);
   }
 
 }
--- src/app/components/team-match/team-match.component.ts
+++ src/app/components/team-match/team-match.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { TeamService } from '../../services/team-service/team.service';
-import * as firebase from "firebase";
-import { MapComponent } from '../map/map.component';
-import { UserService } from '../../services/user-service/user.service';
-import { } from 'googlemaps';
-import { LocationComponent } from '../locations/location/location.component';
-import { MapService } from '../../services/map/map.service';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {TeamService} from '../../services/team-service/team.service';
+import * as firebase from 'firebase';
+import {MapComponent} from '../map/map.component';
+import {UserService} from '../../services/user-service/user.service';
+import {} from 'googlemaps';
+import {LocationComponent} from '../locations/location/location.component';
+import {MapService} from '../../services/map/map.service';
+import {ActivatedRoute} from '@angular/router';
 
 import * as moment from 'moment';
 
@@ -18,25 +18,25 @@ import * as moment from 'moment';
 export class TeamMatchComponent implements OnInit {
 
 
-  private usersTeams:any[] = []; //array of all taems the user is on
-  private allTeams:any[] = [];	//every team in the database
-  private opponents:any[] = [];	//valid opponents for this users team
+  public usersTeams: any[] = []; //array of all taems the user is on
+  public allTeams: any[] = [];	//every team in the database
+  public opponents: any[] = [];	//valid opponents for this users team
   location;	//selected event location
   date;	//date for event
-  error: string = "";
+  error = '';
 
 
   selectedUserTeam; //the team of the user
   selectedOpponent;
   opponentPassedIn = false;
 
-   //map stuff
-   lat: 0;
-   lng: 0;
-   zoom: 5;
-   mapType: string = 'roadmap';
-   passedInLocation;
-   sub:any;
+  //map stuff
+  lat: 0;
+  lng: 0;
+  zoom: 5;
+  mapType = 'roadmap';
+  passedInLocation;
+  sub: any;
 
   constructor(private route: ActivatedRoute, private mapService: MapService, public teamService: TeamService, private userService: UserService) {
     this.selectedUserTeam = null;
@@ -44,15 +44,15 @@ export class TeamMatchComponent implements OnInit {
     this.location = null;
 
 
-    var userCoords = this.userService.userCoords;
+    let userCoords = this.userService.userCoords;
     this.lat = 0;
     this.lng = 0;
 
 
     this.date = new Date().toISOString();
     this.location = {
-      name: "",
-      address: "",
+      name: '',
+      address: '',
     };
 
     this.passedInLocation = null;
@@ -60,204 +60,192 @@ export class TeamMatchComponent implements OnInit {
   }
 
 
-    ngOnInit() {
-      //call teamService = get teams from database
-      this.teamService.downloadTeams()
-        .then((teams: any[]) => {
+  ngOnInit() {
+    //call teamService = get teams from database
+    this.teamService.downloadTeams()
+      .then((teams: any[]) => {
 
 
-          //save all teams into array
-          this.allTeams = teams;
+        //save all teams into array
+        this.allTeams = teams;
 
-          //find users teams
-          for (var i = 0; i < teams.length; i++) {
-            if (this.teamService.userInTeam(teams[i].id)) {
-              this.usersTeams.push(teams[i]);
+        //find users teams
+        for (let i = 0; i < teams.length; i++) {
+          if (this.teamService.userInTeam(teams[i].id)) {
+            this.usersTeams.push(teams[i]);
+          }
+        }
+
+        this.sub = this.route.params.subscribe(params => {
+          let locId = params['locId'];
+          let teamId = params['teamId'];
+
+          if (teamId) {
+            for (let k = 0; k < this.allTeams.length; k++) {
+              if (this.allTeams[k].id == teamId) {
+                this.selectedOpponent = this.allTeams[k];
+              }
             }
           }
 
-          this.sub = this.route.params.subscribe(params => {
-      		var locId = params['locId'];
-      		var teamId = params['teamId'];
+          if (locId) {
+            const loc = this.mapService.getMarkerById(locId);
+            this.passedInLocation = loc;
+            this.lat = loc.coords.lat;
+            this.lng = loc.coords.lng;
 
-      		if (teamId) {
-      			for (var k = 0; k < this.allTeams.length;k++) {
-      				if (this.allTeams[k].id == teamId) {
-      					this.selectedOpponent = this.allTeams[k];
-      				}
-      			}
-      		}
+            this.opponentPassedIn = true;
+          }
 
-      		if (locId) {
-      			var loc = this.mapService.getMarkerById(locId);
-      			this.passedInLocation = loc;
-      			this.lat = loc.coords.lat;
-      			this.lng = loc.coords.lng;
-
-      			this.opponentPassedIn = true;
-      		}
-
-    	   });
+        });
 
 
-
-        })
-
+      });
 
 
+  }
+
+
+  /*
+      Event triggered on mouse click on map. Sends location name,
+      coordinates, address from map
+  */
+  onLocationPicked(location) {
+
+    this.location = location;
+  }
+
+
+  //called when value of user team select changes
+  //when changes, call team service to get a list
+  //of valid opponents for selected team
+  userTeamChanged() {
+    this.opponents = this.teamService.matchmaking(this.selectedUserTeam);
+
+  }
+
+
+  //called when select button on opponent is clicked
+  setOpponent(id) {
+    this.selectedOpponent = id;
+
+    //change background of table for for selected opponent
+    let row = document.getElementById(id);
+    row.style.background = '#8bf27d';
+  }
+
+
+  /*
+      Checks if all fields are given before submitting to database
+  */
+  formIsValid() {
+
+    if (!this.selectedUserTeam || this.selectedUserTeam == '') {
+      this.error = 'You must choose which of your teams to play';
+      return false;
+    }
+
+    if (!this.selectedOpponent || this.selectedOpponent == '') {
+      this.error = 'You must choose an opponent team';
+      return false;
+    }
+
+    if (!this.location || this.location.name == '' || this.location.address == '' ||
+      !this.location.coords || this.location.coords.lat == 0 || this.location.lng == 0) {
+
+      this.error = 'You must choose a location to host the event';
+      return false;
     }
 
 
-    /*
-        Event triggered on mouse click on map. Sends location name,
-        coordinates, address from map
-    */
-    onLocationPicked(location)
-    {
+    this.error = '';
+    return true;
 
-      this.location = location;
-    }
+  }
 
 
-    //called when value of user team select changes
-    //when changes, call team service to get a list
-    //of valid opponents for selected team
-    userTeamChanged()
-    {
-      this.opponents = this.teamService.matchmaking(this.selectedUserTeam);
+  /*
+      Checks if all data given is valid then enters it into database
+  */
+  submit() {
 
-    }
+    if (this.formIsValid()) {
 
+      let usersTeam = this.teamService.getTeamById(this.selectedUserTeam);
 
-    //called when select button on opponent is clicked
-    setOpponent(id)
-    {
-      this.selectedOpponent = id;
-
-      //change background of table for for selected opponent
-      var row = document.getElementById(id);
-      row.style.background = '#8bf27d';
-    }
-
-
-    /*
-        Checks if all fields are given before submitting to database
-    */
-    formIsValid()
-    {
-
-      if (!this.selectedUserTeam || this.selectedUserTeam == "") {
-        this.error = "You must choose which of your teams to play";
-        return false;
+      let oppTeam;
+      if (this.opponentPassedIn == true) {
+        oppTeam = this.selectedOpponent;
+      } else {
+        oppTeam = this.teamService.getTeamById(this.selectedOpponent);
       }
 
-      if (!this.selectedOpponent || this.selectedOpponent == "") {
-        this.error = "You must choose an opponent team";
-        return false;
-      }
 
-      if (!this.location || this.location.name == "" || this.location.address == "" ||
-        !this.location.coords || this.location.coords.lat == 0 || this.location.lng == 0) {
+      let sport = usersTeam.sport;
 
-        this.error = "You must choose a location to host the event";
-        return false;
-      }
+      let players = usersTeam.members.concat(oppTeam.members);
 
 
-      this.error = "";
-      return true;
-
-    }
+      let date = moment(this.date).toISOString();
 
 
-    /*
-        Checks if all data given is valid then enters it into database
-    */
-    submit()
-    {
-
-      if (this.formIsValid()) {
-
-        var usersTeam = this.teamService.getTeamById(this.selectedUserTeam);
-
-		var oppTeam;
-		if (this.opponentPassedIn == true) {
-			oppTeam = this.selectedOpponent;
-		}
-		else {
-        	oppTeam = this.teamService.getTeamById(this.selectedOpponent);
-		}
+      let newMatch = {
+        teams: {
+          teamA: usersTeam,
+          taemB: oppTeam
+        },
+        date: date,
+        finished: false,
+        winner: 'none',
+        scores: {
+          teamA: 0,
+          teamB: 0
+        },
+        players: players
+      };
 
 
-        var sport = usersTeam.sport;
+      //make location object to insert into database
+      let newLocation = {
+        activity: sport,
+        address: this.location.address,
+        coords: this.location.coords,
+        date: date,
+        name: this.location.name,
+        players: players,
+        type: 'team',
+        teams: {
+          teamA: usersTeam,
+          teamB: oppTeam
+        },
+        finished: false,
+        winner: 'none'
+      };
 
-        var players = usersTeam.members.concat(oppTeam.members);
+      if (this.opponentPassedIn == true) {
 
-
-        var date = moment(this.date).toISOString();
-
-
-        var newMatch = {
-          teams: {
-            teamA: usersTeam,
-            taemB: oppTeam
-          },
-          date: date,
-          finished: false,
-          winner: 'none',
-          scores: {
-            teamA: 0,
-            teamB: 0
-          },
-          players: players
+        newLocation = this.passedInLocation;
+        newLocation.teams = {
+          teamA: usersTeam,
+          teamB: oppTeam
         };
+        delete newLocation['score'];
+        delete newLocation['icon'];
+        newLocation.players = usersTeam.members.concat(oppTeam.members);
+        newLocation.date = date;
+        newLocation.finished = false;
 
+        this.teamService.updateMatch(newLocation);
+        return;
 
-        //make location object to insert into database
-        var newLocation = {
-          activity: sport,
-          address: this.location.address,
-          coords: this.location.coords,
-          date: date,
-          name: this.location.name,
-          players: players,
-          type: 'team',
-          teams: {
-            teamA: usersTeam,
-            teamB: oppTeam
-          },
-          finished: false,
-          winner: 'none'
-        };
-
-        if (this.opponentPassedIn == true) {
-
-        	newLocation = this.passedInLocation;
-        	newLocation.teams = {
-        		teamA: usersTeam,
-        		teamB: oppTeam
-        	};
-        	delete newLocation['score'];
-        	delete newLocation['icon'];
-        	newLocation.players =  usersTeam.members.concat(oppTeam.members);
-        	newLocation.date = date;
-        	newLocation.finished = false;
-
-        	this.teamService.updateMatch(newLocation);
-        	return;
-
-        }
-
-
-        this.userService.insertMatch(newMatch, newLocation);
-        this.mapService.addLocation(newLocation);
       }
 
+
+      this.userService.insertMatch(newMatch, newLocation);
+      this.mapService.addLocation(newLocation);
     }
 
-
-
-
+  }
 
 
 }
