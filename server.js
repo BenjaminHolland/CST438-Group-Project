fa0cc9870369201c6f227dd3a3a20e79cb0993af
Benjamin Holland <ben.holland.core@gmail.com>
2018-05-01 21:57:58 -0700

    Respected environment port request

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ NOTE:
 I'm not super up on the object scope for javascript or how express handles apps/servers, and I don't have time to find
 out. I THINK I could remove the references to the servers and be fine.
  */
-const webPort=80;
-const socketPort=3000;
+const webPort = process.env.PORT || 80;
+const socketPort = 3000;
 
-const express=require('express');
+const express = require('express');
 const app = express();
 
 //create and start the angular server
-const angularServer=require('./server/ng-server')
+const angularServer = require('./server/ng-server')
   .boundTo(webPort)
   .usingApp(app)
   .usingExpress(express)
   .start();
 
 //create and start the socket server
-const socketServer=require('./server/socket-server')
+const socketServer = require('./server/socket-server')
 
   .boundTo(socketPort)
   .usingApp(app)
